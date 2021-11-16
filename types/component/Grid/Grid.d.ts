925ef737a9416555692bddb0ed74cca3a8f1c6d6
Deniz Aktürk <deniz.akturk@hotmail.com>
2021-11-16 18:32:48 +0300

    Remove auto generate types folder

--- types/component/Grid/Grid.d.ts
+++
@@ -1,5 +0,0 @@
-import { FC } from 'react';
-import './Grid.css';
-import { GridContainerProps, GridItemProps } from './Grid.types';
-export declare const GridContainer: FC<GridContainerProps>;
-export declare const GridItem: FC<GridItemProps>;
--- types/component/Grid/Grid.types.d.ts
+++
@@ -1,6 +0,0 @@
-export interface GridContainerProps {
-    children: any;
-}
-export interface GridItemProps {
-    children: any;
-}
